31e7c3d702adc2a04090ad928fe4d11108314000
shivang104 <67458972+shivang104@users.noreply.github.com>
2022-09-27 14:26:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,29 @@ app = FastAPI()
 @app.get('/ticker/{ticker_name}')
 async def root(ticker_name):
     yahoo_financials = YahooFinancials(ticker_name)
-    stmnts = yahoo_financials.get_financial_stmts('annual', ['income', 'cash', 'balance'])
-    income_statement = stmnts['incomeStatementHistory'][ticker_name]
-    cash_statement = stmnts['cashflowStatementHistory'][ticker_name]
-    balance_statement = stmnts['balanceSheetHistory'][ticker_name]
-    df_list_is = []
-    df_list_cs = []
-    df_list_bs = []
-    for d in income_statement:
-        df_list_is.append(pd.DataFrame.from_dict(d, orient='index'))
-    for d in cash_statement:
-        df_list_cs.append(pd.DataFrame.from_dict(d, orient='index'))
-    for d in balance_statement:
-        df_list_bs.append(pd.DataFrame.from_dict(d, orient='index'))
-    df_is = pd.concat(df_list_is)
-    df_cs = pd.concat(df_list_cs)
-    df_bs = pd.concat(df_list_bs)
-    is_json = df_is.to_json()
-    cs_json = df_cs.to_json()
-    bs_json = df_bs.to_json()
-    list_json = {}
-    list_json['is'] = is_json
-    list_json['cs'] = cs_json
-    list_json['bs'] = bs_json
-#     json_obj = json.dumps(list_json)
-    return list_json
+    stmnts = yahoo_financials.get_financial_stmts('annual', ['income', 'cash', 'balance'])s
+    return stmnt
+#     income_statement = stmnts['incomeStatementHistory'][ticker_name]
+#     cash_statement = stmnts['cashflowStatementHistory'][ticker_name]
+#     balance_statement = stmnts['balanceSheetHistory'][ticker_name]
+#     df_list_is = []
+#     df_list_cs = []
+#     df_list_bs = []
+#     for d in income_statement:
+#         df_list_is.append(pd.DataFrame.from_dict(d, orient='index'))
+#     for d in cash_statement:
+#         df_list_cs.append(pd.DataFrame.from_dict(d, orient='index'))
+#     for d in balance_statement:
+#         df_list_bs.append(pd.DataFrame.from_dict(d, orient='index'))
+#     df_is = pd.concat(df_list_is)
+#     df_cs = pd.concat(df_list_cs)
+#     df_bs = pd.concat(df_list_bs)
+#     is_json = df_is.to_json()
+#     cs_json = df_cs.to_json()
+#     bs_json = df_bs.to_json()
+#     list_json = {}
+#     list_json['is'] = is_json
+#     list_json['cs'] = cs_json
+#     list_json['bs'] = bs_json
+# #     json_obj = json.dumps(list_json)
+#     return list_json
